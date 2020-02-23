97265495bf93fe3631bbad656961a186e4940c8f
SIonut0122 <sionut0122@yahoo.com>
2020-02-23 19:21:26 +0100

    Updates

---
+++ precache-manifest.b278fb2efbaf8145ce8e8098072466a3.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af1a749c909f1160c9a4103d07932cc",
+    "url": "/fooddelivery/index.html"
+  },
+  {
+    "revision": "d86bf316e8ceb55dbd0e",
+    "url": "/fooddelivery/static/css/main.a21fb967.chunk.css"
+  },
+  {
+    "revision": "8c4d781f88774672be28",
+    "url": "/fooddelivery/static/js/2.ceb262b8.chunk.js"
+  },
+  {
+    "revision": "1bf4d8a008297417532d059b169a9d2a",
+    "url": "/fooddelivery/static/js/2.ceb262b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86bf316e8ceb55dbd0e",
+    "url": "/fooddelivery/static/js/main.3aa5ce08.chunk.js"
+  },
+  {
+    "revision": "a82d780aad8cf2673d77",
+    "url": "/fooddelivery/static/js/runtime-main.d3a31a52.js"
+  },
+  {
+    "revision": "bfec08b33410707a6a92f4166734790a",
+    "url": "/fooddelivery/static/media/artisan_grilled_chicken.bfec08b3.png"
+  },
+  {
+    "revision": "7fd55c305506bcbe338e62b7d42f873d",
+    "url": "/fooddelivery/static/media/bacon.7fd55c30.png"
+  },
+  {
+    "revision": "e40c76f5225f8cbfd97b829cee8dbd5b",
+    "url": "/fooddelivery/static/media/bacon_grilled_chicken_salad.e40c76f5.png"
+  },
+  {
+    "revision": "251855520a2ca4a50df503e7e6660a63",
+    "url": "/fooddelivery/static/media/bacon_salad_chicken.25185552.png"
+  },
+  {
+    "revision": "478b320f5dc1adf9de2459cba22996fc",
+    "url": "/fooddelivery/static/media/bg.478b320f.jpg"
+  },
+  {
+    "revision": "85f75a649dcd6c9d4ba64cb0d03dc1d4",
+    "url": "/fooddelivery/static/media/bg.85f75a64.jpg"
+  },
+  {
+    "revision": "71d24c6a6b9df83299b6913ce297e5a6",
+    "url": "/fooddelivery/static/media/big_bacon_hamb.71d24c6a.png"
+  },
+  {
+    "revision": "8415b4f44638bce8fb9f5e00e5de2e0a",
+    "url": "/fooddelivery/static/media/bord.8415b4f4.png"
+  },
+  {
+    "revision": "1c6a4b0cde64fa6ddb8b7437456f44e7",
+    "url": "/fooddelivery/static/media/bord_two.1c6a4b0c.png"
+  },
+  {
+    "revision": "2154bc6d3eb7c96ee40b468067b9865b",
+    "url": "/fooddelivery/static/media/bun.2154bc6d.png"
+  },
+  {
+    "revision": "43c8b3acb0926fddc8232a7c90481346",
+    "url": "/fooddelivery/static/media/bun.43c8b3ac.png"
+  },
+  {
+    "revision": "4d777663867876c4ca702642bacc62ad",
+    "url": "/fooddelivery/static/media/bun.4d777663.png"
+  },
+  {
+    "revision": "fa7878bdf4d199a100d1a186dce6dd24",
+    "url": "/fooddelivery/static/media/butter.fa7878bd.png"
+  },
+  {
+    "revision": "790b3f4de7cda19582f34ceef55cd76b",
+    "url": "/fooddelivery/static/media/carrot.790b3f4d.png"
+  },
+  {
+    "revision": "1640e0c56d1eecd25e6bd0a5aaee293e",
+    "url": "/fooddelivery/static/media/cheddar_cheese.1640e0c5.png"
+  },
+  {
+    "revision": "82c2eb18e91d710e0c94b225b05a2605",
+    "url": "/fooddelivery/static/media/cheese.82c2eb18.png"
+  },
+  {
+    "revision": "37a6c114fe45da1efad0ae925ff4aa95",
+    "url": "/fooddelivery/static/media/cheesecake.37a6c114.png"
+  },
+  {
+    "revision": "eda599ca9a2873e5a863b5788d56db4b",
+    "url": "/fooddelivery/static/media/chef_img.eda599ca.jpg"
+  },
+  {
+    "revision": "c02f65592f92a13cae2aa21bb1232c3a",
+    "url": "/fooddelivery/static/media/chicken_sandwich.c02f6559.png"
+  },
+  {
+    "revision": "d1ff8f47242b2aed32719d9967c3229c",
+    "url": "/fooddelivery/static/media/chili_lime_tortilla_strips.d1ff8f47.png"
+  },
+  {
+    "revision": "eae0582a3e6f0e55a52e8ad63b6a5a02",
+    "url": "/fooddelivery/static/media/chocolate.eae0582a.png"
+  },
+  {
+    "revision": "5b533700e3d53b797160618b291dfa7f",
+    "url": "/fooddelivery/static/media/chocolate_cake.5b533700.png"
+  },
+  {
+    "revision": "dcdfeffaf6c7fafd041b92e151d78572",
+    "url": "/fooddelivery/static/media/coca_cola.dcdfeffa.png"
+  },
+  {
+    "revision": "4303d5748136b4dfa62dcd535312cc49",
+    "url": "/fooddelivery/static/media/coca_cola_zero.4303d574.png"
+  },
+  {
+    "revision": "70a7f16b1868de331a6ff1b4f3cc7188",
+    "url": "/fooddelivery/static/media/cocoa.70a7f16b.png"
+  },
+  {
+    "revision": "838ace6284d5e4bcac5824ad6ffa5dbb",
+    "url": "/fooddelivery/static/media/corn.838ace62.png"
+  },
+  {
+    "revision": "bd274f496409c4c264db86019d21f799",
+    "url": "/fooddelivery/static/media/creme_caramel.bd274f49.png"
+  },
+  {
+    "revision": "99bdd37fbbe1d1fe01a9c8c885287b12",
+    "url": "/fooddelivery/static/media/cucumber.99bdd37f.png"
+  },
+  {
+    "revision": "a7a1e981d20d5394df01d483c804bc12",
+    "url": "/fooddelivery/static/media/descr_meal.a7a1e981.png"
+  },
+  {
+    "revision": "f36fcd98f3e6977e64ae210a91990ffe",
+    "url": "/fooddelivery/static/media/donuts.f36fcd98.png"
+  },
+  {
+    "revision": "84ed3327195adbb2a947ac1ad1117e0b",
+    "url": "/fooddelivery/static/media/double_beef_hamb.84ed3327.png"
+  },
+  {
+    "revision": "0d016046d81cfd9793cf6bc012c997d6",
+    "url": "/fooddelivery/static/media/eggs.0d016046.png"
+  },
+  {
+    "revision": "282e6d4aef09fcad0734946b20af4e4e",
+    "url": "/fooddelivery/static/media/empty_cart_icon.282e6d4a.png"
+  },
+  {
+    "revision": "50fda7404465ca1ec9a47fb5de988c76",
+    "url": "/fooddelivery/static/media/flavor_img.50fda740.jpg"
+  },
+  {
+    "revision": "d1b07fbeae4ae323428b28c3f971621d",
+    "url": "/fooddelivery/static/media/flour.d1b07fbe.png"
+  },
+  {
+    "revision": "89cf0faa644758e7ee5164670319ca0e",
+    "url": "/fooddelivery/static/media/gelatine.89cf0faa.png"
+  },
+  {
+    "revision": "9a98951b957ae54db098b1f5df6106fe",
+    "url": "/fooddelivery/static/media/grill_seasoning.9a98951b.png"
+  },
+  {
+    "revision": "8e456578236630a57677b1d980408c71",
+    "url": "/fooddelivery/static/media/hamb.8e456578.png"
+  },
+  {
+    "revision": "5652f0aaccb417ba405cd6c37db691d5",
+    "url": "/fooddelivery/static/media/hamb_mini_bg_icon.5652f0aa.png"
+  },
+  {
+    "revision": "687eb56794c79c03329c8339d88a887a",
+    "url": "/fooddelivery/static/media/head_cart_icon_hover.687eb567.png"
+  },
+  {
+    "revision": "f680d0ec40646e44699653583c039ee8",
+    "url": "/fooddelivery/static/media/ice.f680d0ec.png"
+  },
+  {
+    "revision": "4955a8ff4aca7e25b9d7c075acd0ed27",
+    "url": "/fooddelivery/static/media/ice_tea.4955a8ff.png"
+  },
+  {
+    "revision": "a1558f0fffeee2ee29440d45ba44ad7e",
+    "url": "/fooddelivery/static/media/israeli_salad.a1558f0f.png"
+  },
+  {
+    "revision": "967692e57dfad9f431e921f60e6787bd",
+    "url": "/fooddelivery/static/media/juice.967692e5.png"
+  },
+  {
+    "revision": "0cf6613e6a2801a2036d4e0fd82097fe",
+    "url": "/fooddelivery/static/media/ketchup.0cf6613e.png"
+  },
+  {
+    "revision": "157617cb0942a387fe96bffec91795c8",
+    "url": "/fooddelivery/static/media/lettuce.157617cb.png"
+  },
+  {
+    "revision": "4f6f00e7f08b337f72a27031d8ebe3f1",
+    "url": "/fooddelivery/static/media/lettuce.4f6f00e7.png"
+  },
+  {
+    "revision": "5ec49c042746cc1f4c7d60affb1bec2f",
+    "url": "/fooddelivery/static/media/lime_glaze.5ec49c04.png"
+  },
+  {
+    "revision": "90015c19332b2bf435a14b5c8eec635f",
+    "url": "/fooddelivery/static/media/mayonnaise.90015c19.png"
+  },
+  {
+    "revision": "441d8eb81c7c0828d4bab8e9438fdf42",
+    "url": "/fooddelivery/static/media/meat.441d8eb8.png"
+  },
+  {
+    "revision": "5649eb4aaee9b23ee902e279217c0bd5",
+    "url": "/fooddelivery/static/media/meat.5649eb4a.png"
+  },
+  {
+    "revision": "d8f8fc3e540fb28c65f24bd47e794739",
+    "url": "/fooddelivery/static/media/meat.d8f8fc3e.png"
+  },
+  {
+    "revision": "9a6caed4442ca7b13fadac44156b6d9b",
+    "url": "/fooddelivery/static/media/milk.9a6caed4.png"
+  },
+  {
+    "revision": "f56d0268d9941010240c416509ccc350",
+    "url": "/fooddelivery/static/media/mouse_wheel.f56d0268.png"
+  },
+  {
+    "revision": "2793e2a4e95561f65feb98c6067f2615",
+    "url": "/fooddelivery/static/media/olive.2793e2a4.png"
+  },
+  {
+    "revision": "24020e0200b162990ba0bd6098823108",
+    "url": "/fooddelivery/static/media/onions.24020e02.png"
+  },
+  {
+    "revision": "be38b708dfb9adb4228f93cd86274581",
+    "url": "/fooddelivery/static/media/order_conf_img.be38b708.png"
+  },
+  {
+    "revision": "a08e51b93ae15de11abb3d8f7a41e199",
+    "url": "/fooddelivery/static/media/pancakes.a08e51b9.png"
+  },
+  {
+    "revision": "2775d6e26fb5b2c66bf6a553ce834b7f",
+    "url": "/fooddelivery/static/media/panna_cotta.2775d6e2.png"
+  },
+  {
+    "revision": "8c8988fb68e094324b040fee884aee2c",
+    "url": "/fooddelivery/static/media/payment_methods.8c8988fb.jpg"
+  },
+  {
+    "revision": "c15f99ca4a9817d6852f47766cd84340",
+    "url": "/fooddelivery/static/media/pepper.c15f99ca.png"
+  },
+  {
+    "revision": "b1d01230a3c5c93bf1428c286819dcc6",
+    "url": "/fooddelivery/static/media/pickle.b1d01230.png"
+  },
+  {
+    "revision": "0afbbf5e63e2f62e4bcf2f44b87d2484",
+    "url": "/fooddelivery/static/media/salad.0afbbf5e.png"
+  },
+  {
+    "revision": "1fa4bc73de1acaa44a9c9be9c4434266",
+    "url": "/fooddelivery/static/media/salad.1fa4bc73.png"
+  },
+  {
+    "revision": "3b82df0a8d8fec146200073a2bb7bb3f",
+    "url": "/fooddelivery/static/media/salad.3b82df0a.png"
+  },
+  {
+    "revision": "b8b50f8e3e240b2e6879eb6d1944fa56",
+    "url": "/fooddelivery/static/media/salt.b8b50f8e.png"
+  },
+  {
+    "revision": "c6280359e7d3066c6b0536d4f42b4aeb",
+    "url": "/fooddelivery/static/media/sauce.c6280359.png"
+  },
+  {
+    "revision": "0213ee33aba2b755ea8e27a9024ad764",
+    "url": "/fooddelivery/static/media/southwest_grilled_salad.0213ee33.png"
+  },
+  {
+    "revision": "69d0f1d8994c2d5fc8e1f1daeea76508",
+    "url": "/fooddelivery/static/media/sprite.69d0f1d8.png"
+  },
+  {
+    "revision": "b2a10131a13b85025ce26b62982b9135",
+    "url": "/fooddelivery/static/media/steak_burger.b2a10131.png"
+  },
+  {
+    "revision": "6baf7f20c1a99123be26c6d267252e5e",
+    "url": "/fooddelivery/static/media/sugar.6baf7f20.png"
+  },
+  {
+    "revision": "9cc123434cce3a8f55e3b23f73008c4f",
+    "url": "/fooddelivery/static/media/tomato.9cc12343.png"
+  },
+  {
+    "revision": "3c5b0a1e1a13b2a8da7f643bc4075b61",
+    "url": "/fooddelivery/static/media/vanilla.3c5b0a1e.png"
+  },
+  {
+    "revision": "f8384681befe1672b60a6b592bd8a29a",
+    "url": "/fooddelivery/static/media/vegetable_blend.f8384681.png"
+  },
+  {
+    "revision": "65bd9f0db3409e876d4528759d5edae4",
+    "url": "/fooddelivery/static/media/vegetable_salad.65bd9f0d.png"
+  },
+  {
+    "revision": "9e070091c60de1e8ce503fa3e86fcffc",
+    "url": "/fooddelivery/static/media/visa.9e070091.png"
+  },
+  {
+    "revision": "59f3bd2e2e896ae098a23e198bd51e1f",
+    "url": "/fooddelivery/static/media/water.59f3bd2e.png"
+  },
+  {
+    "revision": "e4ca65a923964a0f797c0168c60fec4a",
+    "url": "/fooddelivery/static/media/yeast.e4ca65a9.png"
+  }
+]);
